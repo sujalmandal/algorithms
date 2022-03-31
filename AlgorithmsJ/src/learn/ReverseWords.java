a783560ea0a30507b36c88f3ad7594570e1155ee
Sujal Mandal <ss6sujal@gmail.com>
2022-03-31 20:07:43 +0400

    today I learnt abou counting sort, what a beautiful concept <3

--- AlgorithmsJ/src/learn/ReverseWords.java
+++ AlgorithmsJ/src/learn/ReverseWords.java
@@ -1,11 +1,9 @@
-/* package codechef; // don't place package name! */
+package algo;
 
-import java.util.*;
 import java.lang.*;
-import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class ReverseWords
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
